16047b6f2c22584c9a0cdae64e8067bff32913e0
pro-grammer-SD <geniussantu1983@gmail.com>
2025-06-23 21:28:30 +0530

    up

--- deps/automate.bat
+++ deps/automate.bat
@@ -1,18 +1,23 @@
 @echo off
 
+echo Only Python `deps` will be installed!
+
 echo --Step 1--
 echo Upgrading `pip`...
 python -m pip install --upgrade pip
+echo Upgraded `pip`!
 
 echo --Step 2--
 echo Installing `deps`...
 pip install pipreqs
 pip install -r ./python/requirements.txt
+echo Installing `deps` done!
 
 echo --Step 3--
 echo Writing `deps`...
 ping -n 3 localhost >nul
 pipreqs . --force --encoding=utf-8 --ignore tests --savepath ./python/requirements.txt
+echo Writing `deps` Done!
 
 echo --Step 4--
 echo Done performing `dep` jobs!
